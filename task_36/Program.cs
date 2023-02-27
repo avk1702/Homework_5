7aa1e6baefc1d2cdb88b9c20fb978c855ba06dbb
avk1702 <avk1702@bk.ru>
2023-02-27 12:35:03 +0500

    Решение - ввод массива метод, вычисление без метода

---
+++ task_36/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+int Prompt(string massage)
+{
+  System.Console.Write(massage);
+  string ReadInput = System.Console.ReadLine();
+  int result = int.Parse(ReadInput);
+  return result;
+}
+int[] Array(int Length)
+{
+  int[] array = new int[Length];
+    for (int i = 0; i < Length; i++)
+  {
+    array[i] = new Random().Next(0,99);
+   
+    Console.Write(array[i].ToString() + " ");
+  }
+  return array;
+}
+int Length = Prompt ("Длина массива:  ");
+int[] array = Array(Length);
+
+int sum = 0;
+for (int i = 0; i < Length; i++)
+
+  if (i % 2 != 0)
+  {
+    sum = sum + array[i];
+  }
+
+Console.WriteLine($" Сумма элементов, стоящих на нечётных позициях {sum}");
