5e94577d44a3d715f869dff591d770374a8eee73
avk1702 <avk1702@bk.ru>
2023-02-27 17:39:21 +0500

    Решение - ввод массива метод, вычисление без метода

---
+++ task_38/Program.cs
@@ -0,0 +1,45 @@
+﻿//Задача 38: Задайте массив вещественных чисел 
+//Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+ int Prompt(string massage)
+{
+  System.Console.Write(massage);
+  string ReadInput = System.Console.ReadLine();
+  int result = int.Parse(ReadInput);
+  return result;
+}
+double[] Array(int Length)
+{
+  double[] array = new double[Length];
+    for ( int i = 0; i < Length; i++)
+  {
+    array[i] = new Random().NextDouble() * 100;
+   
+    Console.Write(array[i].ToString("F2") + " " );
+    
+  }
+  return array;
+}
+int Length = Prompt ("Длина массива:  ");
+double[] array = Array(Length);
+
+double min = array[0];
+double max = array[0];
+
+for ( int i = 0; i < Length; i++)
+{
+  if (array[i] < min)
+  {
+    min = array[i];
+  }
+  else if (array[i] > max)
+  {
+    max = array[i];
+  }
+  
+}
+double diffMaxMin = max - min;
+Console.WriteLine("Разница между максимальным и минимальным элементом массива равна  " + diffMaxMin.ToString("F2"));
+
+
