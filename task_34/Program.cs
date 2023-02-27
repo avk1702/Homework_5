a1b05e1c52ec67b9c35700d8b1ab37c1ee595b12
avk1702 <avk1702@bk.ru>
2023-02-27 12:03:45 +0500

    Решение - ввод массива метод, вычисление без метода

---
+++ task_34/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными 
+//трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int Prompt(string massage)
+{
+  System.Console.Write(massage);
+  string ReadInput = System.Console.ReadLine();
+  int result = int.Parse(ReadInput);
+  return result;
+}
+int[] Array(int Length)
+{
+  int[] array = new int[Length];
+    for (int i = 0; i < Length; i++)
+  {
+    array[i] = new Random().Next(100,999);
+   
+    Console.Write(array[i].ToString() + " ");
+  }
+  return array;
+}
+int Length = Prompt ("Длина массива:  ");
+int[] array = Array(Length);
+
+//int CountEven(int[] array)
+//{
+  int count = 0;
+  for (int i = 0; i < Length; i++)
+   
+  if (array[i] % 2 == 0)
+  {
+    count++;
+  }
+    
+    //return count;
+  //Console.WriteLine(count);
+   // Console.WriteLine($"Четных: {CountEven}");
+//}
+//Console.WriteLine(count);
+//int Length = Prompt ("Длина массива:  ");
+//int[] array = Array(Length);
+//Console.WriteLine(count);
+
+Console.WriteLine($" Количество четных чисел в массиве {count}");
